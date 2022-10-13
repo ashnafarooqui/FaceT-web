03231af9446673d54a1da7195eaf697184c10178
ashnafarooqui <89665956+ashnafarooqui@users.noreply.github.com>
2022-10-13 23:35:43 +0500

    Add files via upload

---
+++ About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from './components/HeroSection'
+
+const About = () => {
+  const data = {
+    name: "Ecommerce Store",
+}
+return <HeroSection myData={data} />;
+};
+
+
+export default About
+//rface= react arrow function component export
